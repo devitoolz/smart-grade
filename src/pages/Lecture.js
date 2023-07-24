959a6dc8cebfa4c1cf2817cf681fd8905c9f5143
vitaZ-dev <vitaminzdev@gmail.com>
2023-07-24 20:01:07 +0900

    feat: 학사관리-통합 강의관리 페이지 모달창 기초 레이아웃 세팅

--- src/pages/Lecture.js
+++ src/pages/Lecture.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Lecture = () => {
+  const [display, setDisplay] = useState(false);
+  const handleShowDetail = () => {
+    setDisplay(true);
+  };
+
+  // JSX
   return (
     <div style={{ background: 'aliceblue' }}>
       <>Lecture</>
@@ -10,7 +16,7 @@ const Lecture = () => {
         <input type="text" />
         <button>Q</button>
       </div>
-      <div>
+      <div className="table-area">
         <table style={{ textAlign: 'center' }}>
           <thead>
             {/* 
@@ -50,12 +56,82 @@ const Lecture = () => {
                   <td>30</td>
                   <td>???</td>
                   <td>
-                    <button>상세보기</button>
+                    <button onClick={handleShowDetail}>상세보기</button>
                   </td>
                 </tr>
               ))}
           </tbody>
         </table>
+      </div>
+      {display ? <TempModal setDisplay={setDisplay} /> : <></>}
+    </div>
+  );
+};
+
+const TempModal = ({ setDisplay }) => {
+  const tempStyle = {
+    // background: '#00173C',
+    background: 'rgba(0, 23, 60, 0.7)',
+    // opacity: 0.7,
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  // JSX
+  return (
+    <div style={tempStyle}>
+      <div
+        style={{
+          width: '72%',
+          height: '72%',
+          background: 'white',
+          paddingTop: 40,
+          borderRadius: 20,
+        }}
+      >
+        <button onClick={() => setDisplay(false)}>닫기</button>
+        <p>호텔조리학과</p>
+        <table style={{ border: '1px solid red' }}>
+          <thead>
+            <tr>
+              <th>학번</th>
+              <th>이름</th>
+              <th>성별</th>
+              <th>전공</th>
+              <th>출석</th>
+              <th>중간</th>
+              <th>기말</th>
+              <th>평점</th>
+              <th>최종성적</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array(5)
+              .fill()
+              .map(() => (
+                <tr key={0}>
+                  <td>001</td>
+                  <td>002</td>
+                  <td>003</td>
+                  <td>004</td>
+                  <td>005</td>
+                  <td>006</td>
+                  <td>007</td>
+                  <td>008</td>
+                  <td>009</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+        <div className="pagination" style={{ background: 'pink' }}>
+          <span>1 2 3 4 5 6 7 8 9 </span>
+        </div>
       </div>
     </div>
   );
