06dd13a5c0022a67ec3a79a91ca5033e6df098de
dev.cappp <dev.cappp@gmail.com>
2023-07-25 21:53:26 +0900

    feat: table 수정 작업 중

--- src/pages/LectureRoom.js
+++ src/pages/LectureRoom.js
@@ -1,18 +1,12 @@
 import React from 'react';
 
-import { Lwrap, Title, QuestionWindow, Ltable } from '../styles/LectureRoomCss';
+import { Lwrap, Title, QuestionWindow, Ltable, Bname, Pagenation } from '../styles/LectureRoomCss';
+import SearchBar from '../components/SearchBar';
 const LectureRoom = () => {
   return (
     <Lwrap>
-      <Title>
-        <h2>통합 강의실관리</h2>
-        <button>
-          <p>강의실 추가</p>
-        </button>
-      </Title>
-
-      <QuestionWindow>
-        <label htmlFor="건물명">
+      <SearchBar>
+        <Bname>
           <select name="building" className="building" required>
             <option value="" disabled selected>
               건물명
@@ -21,35 +15,27 @@ const LectureRoom = () => {
             <option value="b관">b관</option>
             <option value="c관">c관</option>
           </select>
-        </label>
-        <label htmlFor="호실">
-          <select name="room" className="room" required>
-            <option value="" disabled selected>
-              호실
-            </option>
-            <option value="301호">301호</option>
-            <option value="302호">302호</option>
-            <option value="303호">303호</option>
-          </select>
-        </label>
-        <div className="protect"></div>
-      </QuestionWindow>
+        </Bname>
+      </SearchBar>
 
       <Ltable>
         <th>번호</th>
         <th>장소</th>
         <th>최대 수용인원</th>
+        <th>관리</th>
         <th>비고</th>
         <tr>
           <td>1</td>
           <td>2</td>
           <td>3</td>
           <td>4</td>
+          <td>5</td>
         </tr>
         <tr>
           <td>5</td>
           <td>6</td>
           <td>7</td>
+          <td>8</td>
           <td>8</td>
         </tr>
         <tr>
@@ -57,14 +43,70 @@ const LectureRoom = () => {
           <td>10</td>
           <td>11</td>
           <td>12</td>
+          <td>12</td>
         </tr>
         <tr>
           <td>13</td>
           <td>14</td>
           <td>15</td>
           <td>16</td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+          <td>16</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>14</td>
+          <td>15</td>
+          <td>16</td>
+          <td>16</td>
         </tr>
       </Ltable>
+      <Pagenation>
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+        <p>10</p>
+      </Pagenation>
     </Lwrap>
   );
 };
--- src/styles/LectureRoomCss.js
+++ src/styles/LectureRoomCss.js
@@ -1,38 +1,8 @@
 import styled from '@emotion/styled';
 
-export const Lwrap = styled.div`
-  width: 100%;
-  padding: 20px 0;
-`;
+export const Lwrap = styled.div``;
 
-export const Title = styled.div`
-  display: flex;
-  justify-content: space-between;
-  h2 {
-    left: 450px;
-    color: #1363df;
-    font-weight: 300;
-  }
-  button {
-    left: 1050px;
-    background: #dff6ff;
-    border: #dff6ff;
-    border-radius: 5px;
-    padding: 10px 20px;
-    p {
-      font-size: 15px;
-    }
-  }
-`;
-
-export const QuestionWindow = styled.div`
-  width: 800px;
-  height: 120px;
-  display: flex;
-  justify-content: space-around;
-  background: #d9d9d9;
-  padding-top: 32px;
-  gap: 10px;
+export const Bname = styled.label`
   .building {
     width: 250px;
     height: 50px;
@@ -48,18 +18,25 @@ export const QuestionWindow = styled.div`
   select option[value=''] {
     display: none;
   }
-  .protect {
-    width: 50px;
-    height: 50px;
-    background: white;
-    border-radius: 50%;
+`;
+
+export const Ltable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 70px;
+  text-align: center;
+  background: #f8f8f8;
+
+  th,
+  td {
+    border: 1px solid #dae8ff;
+    padding: 15px 0;
   }
 `;
-export const Ltable = styled.table`
-  border: 10px;
-  width: 800px;
-  height: 300px;
-  border-color: green;
-  background: skyblue;
-  text-align: center;
+
+export const Pagenation = styled.div`
+  display: flex;
+  justify-content: center;
+  p {
+  }
 `;
