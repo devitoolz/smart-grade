47e234164d2143780edc1057ecd707ecbc9de5fa
vitaZ-dev <vitaminzdev@gmail.com>
2023-07-25 21:12:12 +0900

    feat: 학사관리-통합 강의관리 페이지 강의 승인페이지(임시) 버튼 이벤트 프로토타입 구현

--- src/pages/Approval.js
+++ src/pages/Approval.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import { TableArea } from '../styles/MyStyleCSS';
+import React, { useState } from 'react';
+import { TableArea, TempStyle } from '../styles/MyStyleCSS';
 
 const Approval = () => {
+  const [display, setDisplay] = useState(false);
+  const [contents, setContents] = useState({});
   const handleRejectLecture = () => {
     console.log('Reject');
+    setDisplay(true);
   };
-  const handleAcceptLecture = () => {
+  const handleAcceptLecture = _item => {
     console.log('Accept');
+    setContents(_item);
+    setDisplay(true);
   };
+  const arr = [
+    {
+      lecture: '강의명이 들어갈 장소',
+      temp: '전공',
+      grade: '2',
+      professor: '교수님',
+      time: '14:00~16:00',
+      classroom: '5호관 202호',
+      peopleNum: 30,
+    },
+    {
+      lecture: '데이터베이스',
+      temp: '컴퓨터공학',
+      grade: '3',
+      professor: '박그린',
+      time: '13:00~16:00',
+      classroom: '5호관 202호',
+      peopleNum: 30,
+    },
+  ];
 
   // JSX
   return (
@@ -38,37 +63,54 @@ const Approval = () => {
             </tr>
           </thead>
           <tbody>
-            {Array(5)
-              .fill()
-              .map(() => (
-                <tr key={0}>
-                  <td>001</td>
-                  <td>강의명이 들어갈 장소</td>
-                  <td>전공이름</td>
-                  <td>2</td>
-                  <td>담당교수</td>
-                  <td>14:00~16:00</td>
-                  <td>5호관 202호</td>
-                  <td>30</td>
-                  <td>
-                    <div>
-                      <button onClick={handleAcceptLecture} style={{ background: 'skyblue' }}>
-                        승인
-                      </button>
-                      <button onClick={handleRejectLecture} style={{ background: 'coral' }}>
-                        거절
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
+            {arr.map((item, idx) => (
+              <tr key={idx}>
+                <td>001</td>
+                <td>{item.lecture}</td>
+                <td>{item.temp}</td>
+                <td>{item.grade}</td>
+                <td>{item.professor}</td>
+                <td>{item.time}</td>
+                <td>{item.classroom}</td>
+                <td>{item.peopleNum}</td>
+                <td>
+                  <div>
+                    <button
+                      onClick={() => handleAcceptLecture(item)}
+                      style={{ background: 'skyblue' }}
+                    >
+                      승인
+                    </button>
+                    <button onClick={handleRejectLecture} style={{ background: 'coral' }}>
+                      거절
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </TableArea>
       <div className="pagination" style={{ background: 'pink' }}>
         <span>1 2 3 4 5 6 7 8 9 </span>
       </div>
+      {display ? <TempModal setDisplay={setDisplay} contents={contents} /> : null}
     </div>
+  );
+};
+
+const TempModal = ({ setDisplay, contents }) => {
+  // JSX
+  return (
+    <TempStyle>
+      <div className="modal-box">
+        <p>{contents.lecture}</p>
+        <div>
+          <button onClick={() => setDisplay(false)}>확인</button>
+          <button onClick={() => setDisplay(false)}>취소</button>
+        </div>
+      </div>
+    </TempStyle>
   );
 };
 
