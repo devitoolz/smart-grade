33f4f9217582de3409efae7c2d15b6122971fe1c
dev.cappp <dev.cappp@gmail.com>
2023-08-02 11:07:24 +0900

    feat: major page 모달 작업중

--- src/pages/LectureRoom.js
+++ src/pages/LectureRoom.js
@@ -6,6 +6,10 @@ import SearchBar from '../components/SearchBar';
 import Dropdown from '../components/Dropdown';
 import Input from '../components/Input';
 import CommonButton from '../components/CommonButton';
+import Table from '../components/Table';
+import { Layout } from '../styles/CommonStyle';
+import CommonModal from '../components/CommonModal';
+import { ModalStyle } from '../styles/MyStyleCSS';
 
 const LectureRoom = () => {
   //강의실 추가 모달창 안 호실 인풋창 state
@@ -41,15 +45,13 @@ const LectureRoom = () => {
   return (
     <Lwrap>
       {showModal === true ? <Lwrap /> : null}
-      <SearchBar>
-        <Dropdown placeholder="건물명" />
-      </SearchBar>
+
       <CommonButton btnType="page" value="강의실 추가" onClick={modalOpen} />
 
       {/* modal이 활성화되면 modal을 띄워라 아니면 null  */}
       {showModal === true ? (
         <PlusModal>
-          <div className="LectureLoomTitle">
+          <div className="lectureLoomTitle">
             <p>
               <strong>강의실 추가</strong>
             </p>
@@ -64,7 +66,7 @@ const LectureRoom = () => {
                 <Dropdown placeholder="건물명" />
               </div>
               <div className="inputControl">
-                <Input text="number" length="short" value={roomNumber} setValue={setRoomNumber} />
+                <Input type="number" length="short" value={roomNumber} setValue={setRoomNumber} />
                 <p>호</p>
               </div>
             </div>
@@ -73,7 +75,7 @@ const LectureRoom = () => {
             <p>최대수용인원 : </p>
             <div className="inputControl">
               <Input
-                text="number"
+                type="number"
                 length="short"
                 value={capacityNumber}
                 setValue={setCapacityNumber}
@@ -100,97 +102,11 @@ const LectureRoom = () => {
         </PlusModal>
       ) : null}
 
-      <Ltable>
-        <colgroup>
-          <col className="number" width="7%" />
-          <col className="place" width="30%" />
-          <col className="capacity" width="20%" />
-          <col className="management" width="15%" />
-          <col className="note" width="25%" />
-        </colgroup>
-        <th>번호</th>
-        <th>장소</th>
-        <th>최대 수용인원</th>
-        <th>관리</th>
-        <th>비고</th>
-        {Array(10)
-          .fill()
-          .map((item, idx) => (
-            <tr key={idx}>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>
-                <CommonButton color="red" btnType="table" value="삭제" onClick={bye} />
-              </td>
-              <td>5</td>
-            </tr>
-          ))}
+      <Layout>
+        <SearchBar></SearchBar>
 
-        {/* <tr>
-          <td>5</td>
-          <td>6</td>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-        </tr>
-        <tr>
-          <td>9</td>
-          <td>10</td>
-          <td>11</td>
-          <td>11</td>
-          <td>12</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>14</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>15</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>15</td>
-          <td>16</td>
-        </tr>
-        <tr>
-          <td>13</td>
-          <td>13</td>
-          <td>14</td>
-          <td>15</td>
-          <td>16</td>
-        </tr> */}
-      </Ltable>
+        <Table></Table>
+      </Layout>
       <Pagenation>
         <p>1</p>
         <p>2</p>
--- src/pages/Major.js
+++ src/pages/Major.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 import SearchBar from '../components/SearchBar';
 import Dropdown from '../components/Dropdown';
 import { Ltable, Lwrap, Pagenation, PlusModal } from '../styles/LectureRoomCss';
-
+import Input from '../components/Input';
 import CommonButton from '../components/CommonButton';
 import { useNavigate } from 'react-router-dom';
 const Major = () => {
+  //modalTitle state
+  const [modalTitle, setmodalTitle] = useState('강의실추가', '전공추가');
+  //전공명 state
+  const [majorName, setMajorName] = useState('');
+  //전공학점 state
+  const [majorScore, setMajorScore] = useState(null);
+  //모달창 활성화
+  const [showModal, setshowModal] = useState(false);
+
+  const navigate = useNavigate();
+
+  //버튼 onClick시 모달창 열기
+  const modalOpen = () => {
+    setshowModal(true);
+  };
+
+  //취소, x 누를시 모달창 닫기
+  const modalClose = () => {
+    setshowModal(false);
+  };
+
   const gogo = () => {
     alert('dkdk');
   };
@@ -23,13 +46,26 @@ const Major = () => {
         <Dropdown placeholder="" />
         <Dropdown length="long" placeholder="전공명" />
       </SearchBar>
-      <CommonButton btnType="page" value="전공추가" />
-      <PlusModal>
-        <div ></div>
-        <div></div>
-        <div></div>
-        <div className="btns"></div>
-      </PlusModal>
+      <CommonButton btnType="page" value="전공추가" onClick={modalOpen} />
+      {showModal === true ? (
+        <PlusModal>
+          <div className="majorTitle">
+            <p>
+              <strong> 전공추가 </strong>
+            </p>
+            <p onClick={modalClose}>
+              <FontAwesomeIcon icon={faX} />
+            </p>
+          </div>
+          <div className="majorName">
+            <p>전공명</p>
+            <div>
+              <Input type="text" length="short" value={majorName} setValue={setMajorName}></Input>
+            </div>
+          </div>
+          <div className="btns"></div>
+        </PlusModal>
+      ) : null}
 
       <Ltable>
         <colgroup>
