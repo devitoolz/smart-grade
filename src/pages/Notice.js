6e4ce74d4a92f08e29bdfd02ec62e2e682713299
dev.cappp <dev.cappp@gmail.com>
2023-08-02 20:50:18 +0900

    feat: notice 작업 중

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -3,53 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 import { TableArea } from '../styles/MyStyleCSS';
 import { useNavigate } from 'react-router-dom';
+import Table from '../components/Table';
+import { Layout } from '../styles/CommonStyle';
 
 const Notice = () => {
+  const tableHeader = [];
   const navigate = useNavigate();
   return (
-    <div>
-      <div style={{ borderRadius: 5, width: 80, height: 30, background: 'coral' }}>Notice</div>
-      <TableArea>
-        <table className="notice">
-          <thead>
-            {/* 
-              pink = short
-              yellow = middle
-              lightgreen = 고정
-             */}
-            <tr>
-              <th>NO</th>
-              <th>제목</th>
-              <th>DATE</th>
-              <th>관리</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Array(10)
-              .fill()
-              .map((item, idx) => (
-                <tr key={idx}>
-                  <td>1</td>
-                  <td>중요한 공지사항</td>
-                  <td>2023-07-19</td>
-                  <td>
-                    <button>수정</button>
-                    <button>보기</button>
-                    <button>삭제</button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </TableArea>
-      <button onClick={() => navigate('/home/notice/write')}>
-        글쓰기
-        <FontAwesomeIcon icon={faPen} />
-      </button>
-      <div className="pagination">
-        <span>1 2 3 4 5 6 7 8 9 </span>
-      </div>
-    </div>
+    <Layout>
+      <Table></Table>
+    </Layout>
   );
 };
 
