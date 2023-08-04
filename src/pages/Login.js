de8989db765694a8a8f2f8a205c8dd8368254801
devitoolz <tkdfuf0923@gmail.com>
2023-08-04 10:02:15 +0900

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../components/Input';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const handleLogin = async () => {
+    console.log(id, pw);
+  };
+
+  return (
+    <div>
+      <Input
+        length="long"
+        type="text"
+        placeholder="아이디"
+        reset={setId}
+        value={id}
+        setValue={e => setId(e.target.value)}
+      />
+      <Input
+        length="long"
+        type="text"
+        placeholder="비밀번호"
+        reset={setPw}
+        value={pw}
+        setValue={e => setPw(e.target.value)}
+      />
+      <button onClick={handleLogin}>로그인</button>
+    </div>
+  );
 };
 
 export default Login;
