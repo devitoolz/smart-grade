b462f81061097e319202b74ef77864533a2914a8
vitaZ-dev <vitaminzdev@gmail.com>
2023-08-09 16:24:53 +0900

    feat: 학사관리-통합 성적관리 학생정보 상세보기 데모 구현

--- src/pages/admin/Grade.js
+++ src/pages/admin/Grade.js
@@ -4,9 +4,11 @@ import SearchBar from '../../components/SearchBar';
 import Input from '../../components/Input';
 import Dropdown from '../../components/Dropdown';
 import CommonButton from '../../components/CommonButton';
+import CommonModal from '../../components/CommonModal';
 import Table from '../../components/Table';
 import { getStudentGrade } from '../../api/fetch';
 import useQuerySearch from '../../hooks/useSearchFetch';
+import { FormTable, Row } from '../../styles/UserStyle';
 
 const Grade = () => {
   // 드롭다운
@@ -41,6 +43,7 @@ const Grade = () => {
   const handleGetStudentInfo = async _info => {
     alert('학생 상세정보 불러올 예정');
     console.log(_info);
+    _info ? setDisplay(true) : null;
   };
 
   // 쿼리
@@ -50,6 +53,10 @@ const Grade = () => {
   const { data, pending, error } = useQuerySearch(url, click);
   console.log(error);
   console.log(data);
+
+  //
+  // 학생 상세 정보 모달창
+  const [display, setDisplay] = useState(false);
 
   return (
     <>
@@ -72,19 +79,23 @@ const Grade = () => {
         />
       </SearchBar>
 
-      {/* {!data?.voList?.length ? <NoDatas /> : <></>} */}
-      <CommonButton
-        btnType="page"
-        value="학생상세정보"
-        onClick={() => handleGetStudentInfo(data?.avgVo1?.istudent)}
-      >
-        {/* 학생의 이름+학번?+전공+현재학년 정도 표시(+현재 학점은?) */}
-        {/* {data?.voList[0]?.name} {data?.voList[0]?.studentNum} */}
-        {data?.avgVo1?.name} {data?.avgVo1?.studentNum}
-        {Boolean(data?.avgVo1.length) && (
-          <span style={{ color: 'red' }}>* 학번 입력은 필수입니다</span>
-        )}
-      </CommonButton>
+      {data?.avgVo1 === null ? (
+        <NoDatas />
+      ) : (
+        <CommonButton
+          btnType="page"
+          value="학생상세정보"
+          onClick={() => handleGetStudentInfo(data?.avgVo1?.istudent)}
+        >
+          {/* 학생의 이름+학번?+전공+현재학년 정도 표시(+현재 학점은?) */}
+          {/* {data?.voList[0]?.name} {data?.voList[0]?.studentNum} */}
+          {data?.avgVo1?.name} {data?.avgVo1?.studentNum}
+          {Boolean(data?.avgVo1.length) && (
+            <span style={{ color: 'red' }}>* 학번 입력은 필수입니다</span>
+          )}
+        </CommonButton>
+      )}
+
       <Table
         header={tableHeader}
         data={data?.voList || Array(10).fill('')}
@@ -106,6 +117,47 @@ const Grade = () => {
           );
         })}
       </Table>
+
+      {display && (
+        <CommonModal
+          modalSize="big"
+          modalTitle={`${data.avgVo1.name}님의 상세정보`}
+          setDisplay={setDisplay}
+        >
+          <FormTable>
+            <Row col={2}>
+              <div>이름(name)</div>
+              <div>이름이 들어간다</div>
+              <div>학번(studentNum)</div>
+              <div>9999999</div>
+            </Row>
+            <Row col={2}>
+              <div>성별(gender)</div>
+              <div>남녀</div>
+              <div>학과(majorName)</div>
+              <div>학과=전공</div>
+            </Row>
+            <Row col={2}>
+              <div>입학년도(createdAt)</div>
+              <div>2023-08-09</div>
+              <div>전화번호(phone)</div>
+              <div>012-3456-7890</div>
+            </Row>
+          </FormTable>
+          <div>
+            학생이 들은 학점 = scoreStudent <br />
+            졸업에 필요 학점 = graduationScore
+          </div>
+          <CommonButton
+            btnType="page"
+            value="상세정보"
+            onClick={() => {
+              alert('학생정보 페이지로 넘어감');
+              setDisplay(false);
+            }}
+          />
+        </CommonModal>
+      )}
     </>
   );
 };
