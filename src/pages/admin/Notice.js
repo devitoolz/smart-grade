9cc2407cf50e2d3f8fecfcc1ce181d5b3e1284cb
vitaZ-dev <vitaminzdev@gmail.com>
2023-08-11 15:32:23 +0900

    feat: 공지사항 게시판-게시글 검색 기능 구현중

--- src/pages/admin/Notice.js
+++ src/pages/admin/Notice.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import Table from '../../components/Table';
 import CommonButton from '../../components/CommonButton';
 import SearchBar from '../../components/SearchBar';
@@ -7,21 +7,21 @@ import Input from '../../components/Input';
 import useQuerySearch from '../../hooks/useSearchFetch';
 import CommonModal from '../../components/CommonModal';
 import { deleteBoard } from '../../api/fetch';
-import { async } from 'q';
 
 const Notice = () => {
-  ////SearchBar////
-  //SearchBar queries
-  const queries = {};
   //검색버튼 클릭 state 변경 함수
   const [click, setClick] = useState(false);
   ////Input창////
   //input value값
-  const [noticeTitle, setNoticeTitle] = useState('');
+  // const [noticeTitle, setNoticeTitle] = useState('');
+  const [keyword, setKeyword] = useState('');
   //input value change 함수
   const handleChangeValue = e => {
-    setNoticeTitle(e.target.value);
+    setKeyword(e.target.value);
   };
+  ////SearchBar////
+  //SearchBar queries
+  const queries = { keyword };
 
   //삭제모달 창 활성화 여부
   const [deleteModalShow, setDeleteModalShow] = useState(false);
@@ -37,11 +37,13 @@ const Notice = () => {
     window.location.reload();
   };
 
+  // 공지사항
   // 일반공지+중요공지 같이 불러오기
   // 전체 notice Data 담는 list
   const [noticeData, setNoticeData] = useState([]);
+  const [url, setUrl] = useState('/api/board');
   // custom hook
-  const url = '/api/board';
+  // const url = '/api/board';
   const { data, pending, error } = useQuerySearch(url, click);
   console.log(data);
   // 중요공지
@@ -55,6 +57,31 @@ const Notice = () => {
       ? null
       : setNoticeData([...important.data, ...data.list]);
   }, [data, important.data]);
+  //
+  const [query, setQuery] = useSearchParams();
+  // useEffect(() => {
+  //   keyword ? query.set('keyword', keyword) : null;
+  //   setQuery(query);
+  // }, []);
+  useEffect(() => {
+    query.get('keyword') ? setUrl('/api/board/search') : setUrl('/api/board');
+  }, [url, click, data]);
+  // 게시글 검색
+  // const [query, setQuery] = useSearchParams();
+  // useEffect(() => {
+  //   keyword ? query.set('keyword', keyword) : null;
+  //   setQuery(query);
+  // }, []);
+  // useEffect(() => {
+  //   query.get('keyword');
+  //   setQuery(query);
+  // }, []);
+  // const searchUrl = '/api/board/search';
+  // const searchBoard = useQuerySearch(searchUrl, click);
+  // console.log(searchBoard?.data?.list);
+  // useEffect(() => {
+  //   searchBoard.data.list === [] ? null : setNoticeData([...searchBoard.data.list]);
+  // }, [searchBoard.data.list]);
 
   //notice test list
   // const getNoticeListLoad = async () => {
@@ -93,9 +120,9 @@ const Notice = () => {
           length="long"
           type="text"
           placeholder="제목"
-          value={noticeTitle}
+          value={keyword}
           setValue={handleChangeValue}
-          reset={setNoticeTitle}
+          reset={setKeyword}
           maxLength={20}
         />
       </SearchBar>
@@ -135,7 +162,8 @@ const Notice = () => {
             <div key={item.iboard}>
               <div>{item.importance ? `[중요]` : item.iboard}</div>
               <div>{item.title}</div>
-              <div>{item.createdAt}</div>
+              {/* <div>{item.createdAt}</div> */}
+              <div>{item.createdAt.split('T')[0]}</div>
               <div>
                 <CommonButton
                   btnType="table"
