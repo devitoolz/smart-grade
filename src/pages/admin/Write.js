d18c4ad73e1eeb8eafdf1e5a9f752bf7f8d90707
vitaZ-dev <vitaminzdev@gmail.com>
2023-08-25 18:07:22 +0900

    feat: 관리자 공지사항 글쓰기-toast editor 적용 테스트 중

--- src/pages/admin/Write.js
+++ src/pages/admin/Write.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Ltable, Wbtns } from '../../styles/LectureRoomCss';
 import Input from '../../components/Input';
 import CommonButton from '../../components/CommonButton';
 import CommonModal from '../../components/CommonModal';
 import { useNavigate } from 'react-router-dom';
 import { postBoard } from '../../apis/fetch';
+// toast ui
+import { Editor } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/i18n/ko-kr';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import { Viewer } from '@toast-ui/react-editor';
 
 const Write = () => {
   const navigate = useNavigate();
@@ -23,9 +29,7 @@ const Write = () => {
     navigate('/admin/home/notice/');
   };
   //save modal 취소 버튼 클릭시
-  const saveModalCancel = () => {
-    setSaveDisplay(false);
-  };
+  const saveModalCancel = () => setSaveDisplay(false);
   //cancel modal 확인 버튼 클릭시
   const cancelModalOk = () => navigate('/admin/home/notice/');
   //cancel modal 취소 버튼 클릭시
@@ -38,8 +42,34 @@ const Write = () => {
     const isChecked = document.getElementById('check').checked ? 1 : 0;
     await postBoard(title, boardContents, isChecked);
   };
+
+  // 게시판
+  const editorRef = useRef(null);
   // 이미지 업로드 관련
   const [imgList, setImgList] = useState([]);
+  // 툴바 커스텀
+  const toolbarItems = [
+    ['heading', 'bold', 'italic', 'strike'],
+    ['hr', 'quote'],
+    ['ul', 'ol', 'indent', 'outdent'],
+    ['table', 'image', 'link'],
+  ];
+
+  // 게시글 확인-toast ui test
+  const test = `# markdown
+  ~~***test***~~
+  ing`;
+  // 게시글 작성
+  const handleBoardSave = () => {
+    const markdownContent = editorRef.current?.getInstance().getMarkdown();
+    console.log('게시글 내용');
+    console.log(markdownContent);
+    setSaveDisplay(true);
+  };
+  const handleBoardCancel = () => {
+    setCancelDisplay(true);
+    // navigate() // 이전페이지로 이동
+  };
 
   // JSX
   return (
@@ -66,16 +96,31 @@ const Write = () => {
           <p>취소 하시겠습니까?</p>
         </CommonModal>
       ) : null}
-      <div style={{ width: '100%', height: '100%', padding: 50 }}>
+      <div style={{ width: '100%', height: '100%', padding: '10px 50px 0' }}>
         <div
           style={{
+            height: 50,
             display: 'flex',
             justifyContent: 'space-between',
-            paddingBottom: 18,
+            alignItems: 'center',
             borderBottom: '1px solid black',
           }}
         >
           <h3 style={{ fontSize: 22 }}>게시판 글쓰기</h3>
+          <div style={{ display: 'flex' }}>
+            <CommonButton
+              btnType="table"
+              color="blue"
+              value="저장"
+              onClick={() => handleBoardSave()}
+            />
+            <CommonButton
+              btnType="table"
+              color="red"
+              value="취소"
+              onClick={() => handleBoardCancel()}
+            />
+          </div>
         </div>
         <div style={{ width: 100, height: 20 }}></div>
         <Input
@@ -91,19 +136,30 @@ const Write = () => {
           <span>첨부파일 </span>
         </div>
         <div style={{ width: 100, height: 20 }}></div>
-        <div style={{ width: '100%', height: 560, border: '1px solid gray' }}>
+        {/* <div style={{ width: '100%', height: 560, border: '1px solid gray' }}>
           내용을 입력하세요
-        </div>
-        <Wbtns>
-          <CommonButton
-            btnType="page"
-            value="저장"
-            onClick={() => {
-              setSaveDisplay(true);
-            }}
+        </div> */}
+        <div style={{ padding: 20, border: '1px solid gray' }}>
+          <Editor
+            ref={editorRef}
+            placeholder="내용을 입력하세요"
+            // initialValue=""
+            previewStyle="vertical"
+            height="600px"
+            useCommandShortcut={false}
+            language="ko-KR"
+            toolbarItems={toolbarItems}
+            // hooks={{
+            //   addImageBlobHook: handleUploadImage,
+            // }}
+            // hideModeSwitch={true}
+            // initialEditType="wysiwyg"
+            // viewer={true} // 나중에 다시 살펴보기
           />
-          <CommonButton btnType="page" value="취소" onClick={() => setCancelDisplay(true)} />
-        </Wbtns>
+        </div>
+      </div>
+      <div style={{ width: '100%', height: '100%', padding: '10px 50px 0' }}>
+        <Viewer initialValue={test} />
       </div>
       {/*
       <Ltable>
