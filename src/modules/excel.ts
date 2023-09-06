fd0092d099dbe558f73d5c59f1fe3b032a6e4581
devitoolz <tkdfuf0923@gmail.com>
2023-09-06 11:35:23 +0900

    feat: 엑셀 업로드 다운로드 모듈화 및 학생 계정 일괄 생성 추가

---
+++ src/modules/excel.ts
@@ -0,0 +1,185 @@
+import { Workbook } from 'exceljs';
+import { ObjectType } from '../types/components';
+import { saveAs } from 'file-saver';
+import React from 'react';
+import { read, utils } from 'xlsx';
+
+const handleDownloadExcel = async (
+  role: 'professor' | 'student',
+  sheetName: string,
+  headerWidths: Array<number>,
+  majorList: Array<ObjectType>
+) => {
+  try {
+    const wb = new Workbook();
+    const ws = wb.addWorksheet(sheetName);
+    const ws2 = wb.addWorksheet('데이터', { state: 'hidden' });
+
+    majorList.forEach((item, index) => {
+      ws2.getCell('A' + (1 + index)).value = item.majorName;
+    });
+    ws2.getCell('B1').value = role;
+
+    const headerRow = ws.addRow([
+      '이름',
+      '성별',
+      '생년월일(YYYYMMDD)',
+      '전공',
+      '휴대전화(010, - 제외 8자리)',
+    ]);
+    headerRow.height = 30;
+    headerRow.eachCell((cell, colNum) => {
+      cell.fill = {
+        type: 'pattern',
+        pattern: 'solid',
+        fgColor: { argb: 'FFFFFF00' },
+      };
+      cell.font = {
+        bold: true,
+      };
+      cell.border = {
+        top: { style: 'thin' },
+        left: { style: 'thin' },
+        bottom: { style: 'thin' },
+        right: { style: 'thin' },
+      };
+      cell.alignment = {
+        vertical: 'middle',
+        horizontal: 'center',
+      };
+      ws.getColumn(colNum).width = headerWidths[colNum - 1];
+    });
+
+    //@ts-ignore
+    ws.dataValidations.add('B2:B9999', {
+      type: 'list',
+      allowBlank: true,
+      formulae: ['"남,여"'],
+    });
+
+    // @ts-ignore
+    ws.dataValidations.add('D2:D9999', {
+      type: 'list',
+      allowBlank: true,
+      formulae: [`데이터!$A$1:$A$${majorList.length}`],
+    });
+
+    const fileData = await wb.xlsx.writeBuffer();
+    const blob = new Blob([fileData], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
+    saveAs(blob, `${sheetName} 양식`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleUploadExcel = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  role: 'professor' | 'student',
+  majorList: Array<ObjectType>,
+  errorHandler: React.Dispatch<React.SetStateAction<boolean>>,
+  dataSetter: React.Dispatch<React.SetStateAction<ObjectType[] | null>>
+) => {
+  const file = (e.target.files as FileList)[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onload = () => {
+      if (!e.target) return;
+      const data = reader.result;
+      const file = read(data, {
+        type: 'buffer',
+      });
+      const sheetName = file.SheetNames[0];
+      const rawData = file.Sheets[sheetName];
+      const result: Array<ObjectType> = utils.sheet_to_json(rawData);
+
+      const roleData = file.Sheets[file.SheetNames[1]]['B1'].v;
+
+      if (!roleData || roleData !== role) {
+        alert('올바른 양식을 업로드 해주세요.');
+        return;
+      }
+
+      if (result.length === 0) {
+        alert('생성할 계정이 없습니다.');
+        e.target.value = '';
+        return;
+      }
+
+      const newResult = result.map((item, index) => {
+        return {
+          index,
+          nm: item['이름'],
+          gender: (item['성별'] === '남' && 'M') || (item['성별'] === '여' && 'F'),
+          birthdate: item['생년월일(YYYYMMDD)']
+            .toString()
+            .replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
+          phone: '010'
+            .concat(item['휴대전화(010, - 제외 8자리)'])
+            .replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'),
+          imajor: majorList.find(major => major.majorName === item['전공'])?.imajor,
+          majorName: item['전공'],
+        };
+      });
+
+      let nameHasError: Array<ObjectType> = [];
+
+      newResult.forEach(item => {
+        if (!/^[ㄱ-ㅎ가-힣a-zA-Z\s]+$/.test(item.nm)) {
+          nameHasError.push({ index: item.index, nm: item.nm, error: '이름 오류' });
+        }
+        if (!['M', 'F'].includes(item.gender as string)) {
+          nameHasError.push({ index: item.index, nm: item.nm, error: '성별 오류' });
+        }
+        if (!/(\d{4})-(\d{2})-(\d{2})/.test(item.birthdate)) {
+          nameHasError.push({ index: item.index, nm: item.nm, error: '생년월일 오류' });
+        }
+        if (!majorList.find(major => major.majorName === item.majorName)) {
+          nameHasError.push({ index: item.index, nm: item.nm, error: '전공 오류' });
+        }
+        if (!/(\d{3})-(\d{4})-(\d{4})/.test(item.phone)) {
+          nameHasError.push({ index: item.index, nm: item.nm, error: '휴대전화 오류' });
+        }
+      });
+
+      if (nameHasError.length !== 0) {
+        errorHandler(true);
+
+        const output = Object.values(
+          nameHasError.reduce((res, obj) => {
+            if (!res[obj.nm]) {
+              res[obj.nm] = { ...obj, error: [] };
+            }
+            res[obj.nm].error.push(obj.error);
+            return res;
+          }, {})
+        );
+
+        output.forEach(item => {
+          item.error = item.error.join(', ');
+        });
+
+        const newOutput = newResult.map(result => {
+          for (let i in output) {
+            if (output[i].index === result.index) {
+              return { ...result, ...output[i] };
+            }
+          }
+          return result;
+        });
+
+        e.target.value = '';
+        dataSetter(newOutput);
+        return;
+      }
+
+      e.target.value = '';
+      dataSetter(newResult);
+    };
+  }
+};
+
+export { handleDownloadExcel, handleUploadExcel };
--- src/pages/admin/Professor.tsx
+++ src/pages/admin/Professor.tsx
@@ -10,11 +10,8 @@ import CommonButton from '../../components/CommonButton';
 import { RootState } from '../../store';
 import { ObjectType } from '../../types/components';
 import { Button, ButtonBarLayout } from '../../styles/ButtonBarStyle';
-
-import { saveAs } from 'file-saver';
-import { Workbook } from 'exceljs';
-import { read, utils } from 'xlsx';
 import ExcelData from '../../components/ExcelData';
+import { handleDownloadExcel, handleUploadExcel } from '../../modules/excel';
 
 const Professor = () => {
   const navigate = useNavigate();
@@ -56,165 +53,6 @@ const Professor = () => {
   const viewData = ['nm', 'gender', 'birthdate', 'majorName', 'phone'];
   const postData = ['nm', 'gender', 'birthdate', 'imajor', 'phone'];
 
-  const handleDownloadExcel = async () => {
-    try {
-      const wb = new Workbook();
-      const ws = wb.addWorksheet('교수 계정 등록');
-      const ws2 = wb.addWorksheet('전공', { state: 'hidden' });
-
-      allMajorList.forEach((item, index) => {
-        ws2.getCell('A' + (1 + index)).value = item.majorName;
-      });
-
-      const headerRow = ws.addRow([
-        '이름',
-        '성별',
-        '생년월일(YYYYMMDD)',
-        '전공',
-        '휴대전화(010, - 제외 8자리)',
-      ]);
-      headerRow.height = 30;
-      headerRow.eachCell((cell, colNum) => {
-        cell.fill = {
-          type: 'pattern',
-          pattern: 'solid',
-          fgColor: { argb: 'FFFFFF00' },
-        };
-        cell.font = {
-          bold: true,
-        };
-        cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' },
-        };
-        cell.alignment = {
-          vertical: 'middle',
-          horizontal: 'center',
-        };
-        ws.getColumn(colNum).width = headerWidths[colNum - 1];
-      });
-
-      //@ts-ignore
-      ws.dataValidations.add('B2:B9999', {
-        type: 'list',
-        allowBlank: true,
-        formulae: ['"남,여"'],
-      });
-
-      // @ts-ignore
-      ws.dataValidations.add('D2:D9999', {
-        type: 'list',
-        allowBlank: true,
-        formulae: [`전공!$A$1:$A$${allMajorList.length}`],
-      });
-
-      const fileData = await wb.xlsx.writeBuffer();
-      const blob = new Blob([fileData], {
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-      });
-      saveAs(blob, `교수 계정 등록 양식`);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleUploadExcel = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = (e.target.files as FileList)[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.readAsArrayBuffer(file);
-
-      reader.onload = () => {
-        if (!e.target) return;
-        const data = reader.result;
-        const fileInformation = read(data, {
-          type: 'buffer',
-        });
-        const sheetName = fileInformation.SheetNames[0];
-        const rawData = fileInformation.Sheets[sheetName];
-        const result: Array<ObjectType> = utils.sheet_to_json(rawData);
-
-        if (result.length === 0) {
-          alert('생성할 계정이 없습니다.');
-          e.target.value = '';
-          return;
-        }
-
-        const newResult = result.map((item, index) => {
-          return {
-            index,
-            nm: item['이름'],
-            gender: (item['성별'] === '남' && 'M') || (item['성별'] === '여' && 'F'),
-            birthdate: item['생년월일(YYYYMMDD)']
-              .toString()
-              .replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3'),
-            phone: '010'
-              .concat(item['휴대전화(010, - 제외 8자리)'])
-              .replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'),
-            imajor: allMajorList.find(major => major.majorName === item['전공'])?.imajor,
-            majorName: item['전공'],
-          };
-        });
-
-        let nameHasError: Array<ObjectType> = [];
-
-        newResult.forEach(item => {
-          if (!/^[ㄱ-ㅎ가-힣a-zA-Z\s]+$/.test(item.nm)) {
-            nameHasError.push({ index: item.index, nm: item.nm, error: '이름 오류' });
-          }
-          if (!['남', '여'].includes(item.gender as string)) {
-            nameHasError.push({ index: item.index, nm: item.nm, error: '성별 오류' });
-          }
-          if (!/(\d{4})-(\d{2})-(\d{2})/.test(item.birthdate)) {
-            nameHasError.push({ index: item.index, nm: item.nm, error: '생년월일 오류' });
-          }
-          if (!allMajorList.find(major => major.majorName === item.majorName)) {
-            nameHasError.push({ index: item.index, nm: item.nm, error: '전공 오류' });
-          }
-          if (!/(\d{3})-(\d{4})-(\d{4})/.test(item.phone)) {
-            nameHasError.push({ index: item.index, nm: item.nm, error: '휴대전화 오류' });
-          }
-        });
-
-        if (nameHasError.length !== 0) {
-          setExcelDataHasError(true);
-
-          const output = Object.values(
-            nameHasError.reduce((res, obj) => {
-              if (!res[obj.nm]) {
-                res[obj.nm] = { ...obj, error: [] };
-              }
-              res[obj.nm].error.push(obj.error);
-              return res;
-            }, {})
-          );
-
-          output.forEach(item => {
-            item.error = item.error.join(', ');
-          });
-
-          const newOutput = newResult.map(result => {
-            for (let i in output) {
-              if (output[i].index === result.index) {
-                return { ...result, ...output[i] };
-              }
-            }
-            return result;
-          });
-
-          setExcelData(newOutput);
-          e.target.value = '';
-          return;
-        }
-
-        setExcelData(newResult);
-        e.target.value = '';
-      };
-    }
-  };
-
   return (
     <>
       <SearchBar queries={queries} setPage={true} setClick={setClick}>
@@ -238,9 +76,22 @@ const Professor = () => {
         />
       </SearchBar>
       <ButtonBarLayout>
-        <Button onClick={handleDownloadExcel}>양식 다운로드</Button>
+        <Button
+          onClick={() =>
+            handleDownloadExcel('professor', '교수 계정 등록', headerWidths, allMajorList)
+          }
+        >
+          양식 다운로드
+        </Button>
         <label htmlFor="file">일괄 계정 생성 (엑셀)</label>
-        <input id="file" type="file" accept=".xlsx, .xls" onChange={handleUploadExcel} />
+        <input
+          id="file"
+          type="file"
+          accept=".xlsx, .xls"
+          onChange={e =>
+            handleUploadExcel(e, 'professor', allMajorList, setExcelDataHasError, setExcelData)
+          }
+        />
         <Button onClick={() => navigate('/admin/user/create', { state: 'professor' })}>
           계정 생성
         </Button>
