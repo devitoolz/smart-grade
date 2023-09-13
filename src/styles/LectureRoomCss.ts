ed3f362c39806cec6d083692cf6513917e5c01dd
dev.cappp <dev.cappp@gmail.com>
2023-09-13 15:21:41 +0900

    style: 학생 강의조회 css수정

--- src/styles/LectureRoomCss.ts
+++ src/styles/LectureRoomCss.ts
@@ -441,6 +441,7 @@ export const StudentLectureDetail = styled.div`
       img {
         object-fit: cover;
         width: 100%;
+        height: 100%;
       }
       .icon {
         display: flex;
